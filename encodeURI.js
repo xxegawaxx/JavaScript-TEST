bff51e9b3c6a5904f8489248b94eb87e9ac93fac
xxegawaxx <e.masashi.st@gmail.com>
2020-12-16 03:12:57 +0900

    encodeURI

---
+++ encodeURI.js
@@ -0,0 +1,5 @@
+const uri =
+  'https://www.amazon.co.jp/s?k=react&i=digital-text&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&ref=nb_sb_noss_1';
+
+console.log(encodeURI(uri));
+console.log(decodeURI(uri));
